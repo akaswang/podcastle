0a3474207ea6d701f54c213014d2a3a3071e3bc5
Akash Dewangan <123660848+akashdewgn@users.noreply.github.com>
2024-11-21 07:06:46 +0530

    initial structure

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://hopeful-turtle-75.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
